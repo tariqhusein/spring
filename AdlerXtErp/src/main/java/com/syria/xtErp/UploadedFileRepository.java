4e6ef252f77a227989294ecb679dbb4adb80890e
tariq <tariq88@live.com>
2018-05-20 14:37:33 +0300

    77

--- AdlerXtErp/src/main/java/com/syria/xtErp/UploadedFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.syria.xtErp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UploadedFileRepository extends JpaRepository<UploadedFile, Long> {
-
-}
--- AdlerXtErp/src/main/java/com/syria/xtErp/importData/ImportableEntities.java
+++
@@ -1,6 +0,0 @@
-package com.syria.xtErp.importData;
-
-public enum ImportableEntities {
-	Client,Article,Contact,Adress,Salary,Buying_Price,Selling_Price,Attachment
-
-}
